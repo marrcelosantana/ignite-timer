6e07efb109e17c2f09ca6dccb1bc7b72263ad93b
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-21 16:22:38 -0300

    [Ignite-Timer] - Insert Routes, Componentens and Stylization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global";
+import { Router } from "./routes/Router";
+import { BrowserRouter } from "react-router-dom";
 
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
